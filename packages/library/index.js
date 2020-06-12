89528e0b9975e8a592026befd79f3ad797b67c7b
Gevik Babakhani <gevik@blendjs.com>
2020-06-12 19:17:25 +0200

    perf: added addNumbers

--- packages/library/index.js
+++ packages/library/index.js
@@ -2,11 +2,16 @@ function sayHello (name) {
     console.log(`Hello ${name}!`);
 }
 
-function sayTime() {
-    console.log(`It is ${new Date()} now`)
+function sayTime () {
+    console.log(`It is ${new Date()} now`);
+}
+
+function addNumbers (a, b) {
+    return a + b;
 }
 
 module.exports = {
     sayHello,
-    sayTime
-}
+    sayTime,
+    addNumbers
+};
